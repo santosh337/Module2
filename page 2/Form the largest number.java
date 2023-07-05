beba361ad4c17146161f6e8b48cdbee42f1f335f
Santosh Sugur <santosh.iosdev@gmail.com>
2023-07-05 16:37:23 +0000

    Create Form the largest number.java

---
+++ page 2/Form the largest number.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        int [] arr = new int[n];
+        
+        for (int j=0;j<n;j++) {
+            arr[j] = scn.nextShort();
+        }
+        
+        String str[] = new String[n];
+        
+        for (int i=0;i<n;i++) {
+            str[i] = "" + arr[i];
+        }
+        
+        Arrays.sort(str,(a,b) -> {
+            String s = a + b;
+            String r = b + a;
+            return r.compareTo(s);
+        });
+        String ans = "";
+        
+        for (int a =0;a<n;a++) {
+            ans += str[a];
+        }
+        System.out.println(ans);
+    }
+}
