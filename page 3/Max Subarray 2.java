5d413276ea51f349745fae1134bbd4f6b4c82e68
Santosh Sugur <santosh.iosdev@gmail.com>
2023-07-05 16:41:18 +0000

    Create Max Subarray 2.java

---
+++ page 3/Max Subarray 2.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i=0;i<n;i++) {
+            arr[i] = scn.nextInt();
+        }
+        
+        int sum = 0;
+        int ans = Integer.MIN_VALUE;
+        
+        for (int a=0;a<n;a++) {
+            if (sum < 0) {
+                sum = arr[a];
+            } else {
+                sum += arr[a];
+            }
+            ans = Math.max(ans,sum);
+        }
+        System.out.print(ans);
+    }
+}
