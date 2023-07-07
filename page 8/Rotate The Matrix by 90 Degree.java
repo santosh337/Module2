93f5355ae15cfedadeaf171ecfbf4ba704264fed
Santosh Sugur <santosh.iosdev@gmail.com>
2023-07-07 12:48:12 +0000

    Create Rotate The Matrix by 90 Degree.java
    Take a square matrix of size n*n as input, and rotate the matrix by 90 degree.
    
    NOTE:- After answering the question, attempt the related question in the linked resource to improve your understanding of this question . Click here
    
    Input Format
    
    First line contains Integer N depicting the size of matrix.
    
    Second line contains N * N Integer values, depicting all the elements of matrix.

---
+++ page 8/Rotate The Matrix by 90 Degree.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    public static void transpose(int[][] twodm) {
+        int n = twodm.length;
+        for (int i=0;i<n;i++) {
+            for (int j=0;j<n;j++) {
+                if (i<j) {
+                    int temp = twodm[i][j];
+                    twodm[i][j] = twodm[j][i];
+                    twodm[j][i] = temp;
+                }
+            }
+        }
+    }
+    
+    public static void reverse1D(int[] d) {
+        int i = 0;
+        int j = d.length - 1;
+        while (i<j) {
+            int temp = d[i];
+            d[i] = d[j];
+            d[j] = temp;
+            i++;
+            j--;
+        }
+        
+    }
+    
+    public static void rotate(int[][] Q) {
+        transpose(Q);
+        for (int [] d :Q) {
+            reverse1D(d);
+        }
+    }
+    
+    public static void print_array(int[][] twodm) {
+        for (int i = 0; i < twodm.length; i++) {
+            for (int j = 0; j < twodm[0].length; j++) {
+                System.out.print(twodm[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int k = scn.nextInt();
+        int[][] Q = new int[k][k];
+        
+        for (int i=0;i<k;i++) {
+            for (int j=0;j<k;j++) {
+                Q[i][j] = scn.nextInt();
+            }
+        }
+        rotate(Q);
+        print_array(Q);
+    }
+}
