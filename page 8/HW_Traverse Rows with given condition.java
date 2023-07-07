1c7cb6b8654015150d06c6775f9247694d515a2f
Santosh Sugur <santosh.iosdev@gmail.com>
2023-07-07 12:49:55 +0000

    Create HW_Traverse Rows with given condition.java

---
+++ page 8/HW_Traverse Rows with given condition.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int m = scn.nextInt();
+        int n = scn.nextInt();
+        int[][] arr = new int[m][n];
+        
+        for (int i=0;i<m;i++) {
+            for (int j=0;j<n;j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        
+        for (int i=0;i<m;i++) {
+            if (i%2==0) {
+                for (int j=0;j<n;j++) {
+                    System.out.print(arr[i][j]+" ");
+                }
+            } else {
+                for (int j=n-1;j>=0;j--) {
+                    System.out.print(arr[i][j]+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
