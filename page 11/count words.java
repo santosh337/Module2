a50e0066bebbbf44a3fb15bba6a1e59342fa519b
Santosh Sugur <santosh.iosdev@gmail.com>
2023-07-12 07:00:00 +0000

    Create count words.java
    Samantha was a college student majoring in English literature. One day, her professor assigned the class a writing exercise where they had to write a short story. Samantha was excited about the challenge, but she wasn't sure how to keep track of the word count as she wrote.
    
    Can you create a program that can count the number of words present in the Samantha's short story.
    
    Input Format
    
    Input contains a String str.
    
    Constraints
    
    1<=Str.length()<=100000
    Output Format
    
    Return An integer value.
    
    Sample Input 0
    
    Welcome to geekster
    Sample Output 0
    
    3
    Explanation 0
    
    3 words are present in string i.e welcome, to, geekster.

---
+++ page 11/count words.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str1 = scn.nextLine();
+        int count = 0;
+        String[] strArr = str1.split("\\s+");
+        
+        // for (int i=0;i<str1.length();i++) {
+        //     char ch1 = str1.charAt(i);
+        //     if (ch1 == ' ') {
+        //         count++;
+        //     }
+        // }
+        
+        
+         System.out.println(strArr.length);
+    }
+}
