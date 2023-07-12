fd094867940c6b537f4a3816b3d36ebdf1073172
Santosh Sugur <santosh.iosdev@gmail.com>
2023-07-12 06:58:24 +0000

    Create print characters.java
    Amy is a high school student who is passionate about coding. One day, her computer science teacher gives the class an assignment to print all the characters of a given string in separate lines.
    
    Amy immediately gets to work and writes a simple program. However, she feels that her solution is too basic and wants to find a more efficient way to solve the problem.
    
    can you help Amy by writing a program thst print all the characters of a given string in separate lines.
    
    Input Format
    
    Input contains a String.
    
    Constraints
    
    1<=str.length<=10000
    Output Format
    
    Print each character in differnt line
    
    Sample Input 0
    
    String
    Sample Output 0
    
    S
    t
    r
    i
    n
    g

---
+++ page 11/print characters.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        
+        for (int i=0;i<str.length();i++) {
+            char ch = str.charAt(i);
+            System.out.println(ch);
+        }
+        
+    }
+}
